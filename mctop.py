1743fc642f5079aec3a683b2990f9b4178ed8474
Robb Shecter <robb@weblaws.org>
2013-12-17 15:48:30 -0500

    Very basic screen control

--- mctop.py
+++ mctop.py
@@ -3,8 +3,8 @@
 #
 
 import time
-# See https://github.com/dlrust/python-memcached-stats
-from memcached_stats import MemcachedStats
+from blessings import Terminal              # https://pypi.python.org/pypi/blessings/
+from memcached_stats import MemcachedStats  # https://github.com/dlrust/python-memcached-stats
 
 # Configuration
 interval = 3
@@ -19,8 +19,8 @@ def percent_change_in_interval(a0, a1, b0, b1):
         return '-'
 
 
-# First version: Do a simple repeating report
 print "Interval is", interval, "seconds. Ctrl-c to quit."
+t  = Terminal()
 mc = MemcachedStats()
 time_0   = mc.stats()
 hits_0   = int(time_0['get_hits'])
@@ -30,6 +30,7 @@ time_a = time_0
 while True:
     try:
         time.sleep(interval)
+        print t.clear()
         time_b = mc.stats()
 
         hits_a   = int(time_a['get_hits'])
@@ -39,25 +40,25 @@ while True:
         
         print 'Efficiency'
         try:
-            print("  Interval:     {:>7.1%}".format(percent_change_in_interval(hits_a, hits_b, misses_a, misses_b)))
+            print("  Interval:      {:>7.1%}".format(percent_change_in_interval(hits_a, hits_b, misses_a, misses_b)))
         except:
-            print("  Interval:      ----")
+            print("  Interval:       ----")
 
         try:
-            print("  Cumulative:   {:>7.1%}".format(percent_change_in_interval(hits_0, hits_b, misses_0, misses_b)))
+            print("  Cumulative:    {:>7.1%}".format(percent_change_in_interval(hits_0, hits_b, misses_0, misses_b)))
         except:
-            print('  Cumulative:    ----')
+            print('  Cumulative:     ----')
 
         try:
-            print("  Total:        {:>7.1%}".format(percent_change_in_interval(0, hits_b, 0, misses_b)))
+            print("  Total:         {:>7.1%}".format(percent_change_in_interval(0, hits_b, 0, misses_b)))
         except:
-            print('  Total:         ----')
+            print('  Total:          ----')
 
 
         print 'Requests'
-        print "  Interval:   {0:>6}".format((hits_b - hits_a) + (misses_b - misses_a))
-        print "  Cumulative: {0:>6}".format((hits_b - hits_0) + (misses_b - misses_0))
-        print "  Total:      {0:>6}".format(hits_b + misses_b)
+        print "  Interval:   {0:>7,}".format((hits_b - hits_a) + (misses_b - misses_a))
+        print "  Cumulative: {0:>7,}".format((hits_b - hits_0) + (misses_b - misses_0))
+        print "  Total:      {0:>7,}".format(hits_b + misses_b)
         
         print " "
         
